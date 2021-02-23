6023473bcd89ed26b333c7f44d95770ce2776ba1
zengzefeng <986510453@qq.com>
2021-02-23 16:54:16 +0800

    增加注释

--- src/main/java/cn/iocoder/dashboard/framework/dataauth/package-info.java
+++ src/main/java/cn/iocoder/dashboard/framework/dataauth/package-info.java
@@ -3,7 +3,6 @@ package cn.iocoder.dashboard.framework.dataauth;
  * 数据权限限制SQL自动注入
  * <p>
  * 1、利用mp的mapper扫描将带有需要限制和不需要限制的类/方法信息缓存。
- *
  * @see cn.iocoder.dashboard.framework.dataauth.core.component.DataAuthSqlInjector
  * <p>
  * 2、利用mp的拦截器对执行的方法拦截，并通过1中的信息判断是否需要进行限制。
@@ -16,11 +15,14 @@ package cn.iocoder.dashboard.framework.dataauth;
  * 4、在系统初始化完成后，加载权限限制的表字短信，并全局缓存。
  * @see cn.iocoder.dashboard.framework.dataauth.core.component.DataAuthCacheHelper
  * @see cn.iocoder.dashboard.framework.dataauth.core.entity.DataAuthCache
- * @see cn.iocoder.dashboard.framework.dataauth.impl.DataAuthCacheInit
  * <p>
+ *
  * 使用：
- * 1、添加@DataAuth注解
+ * 1、配置
+ * @see cn.iocoder.dashboard.framework.dataauth.impl.DataAuthCacheInit
+ * @see cn.iocoder.dashboard.framework.dataauth.impl.MyDataAuthStrategy
+ * @see cn.iocoder.dashboard.framework.dataauth.impl.MyRoleDataAuthSqlHandler
+ * 2、添加@DataAuth注解
  * @see cn.iocoder.dashboard.modules.system.dal.mysql.user.SysUserMapper#selectPage(cn.iocoder.dashboard.modules.system.controller.user.vo.user.SysUserPageReqVO, java.util.Collection)
- * 2、登录后请求对应接口即可。
- * 3、日志中有输出前后sql
+ * 3、登录后请求对应接口即可, 日志中有输出前后sql
  */
